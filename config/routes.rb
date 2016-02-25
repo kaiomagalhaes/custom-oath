979d6c9c40ef5fef2f388d3bb3d4ef19bcbb7855
Kaio Magalhaes <me@kaiomagalhaes.com>
2016-02-25 14:38:24 -0300

    add doorkepper gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  use_doorkeeper
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create]
   delete '/logout', to: 'sessions#destroy', as: :logout
